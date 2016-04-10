17beea50f67df1f0c8039be25a70ca8a5526e6be
Anderson Aguiar <revsoft10@gmail.com>
2016-04-10 15:40:45 -0300

    Set new gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,37 @@
-const gulp = require('gulp');
-const stylus = require('gulp-stylus');
-const minifyCss = require('gulp-minify-css');
-const shell = require('gulp-shell');
+const gulp = require('gulp')
+const stylus = require('gulp-stylus')
+const minifyCss = require('gulp-minify-css')
+const shell = require('gulp-shell')
+const image = require('gulp-image')
 
 var paths = {
   stylus: {
     src: ['./public/assets/stylus/**/*.styl'],
     dist: './public/dist/css'
+  },
+  images: {
+    src: ['./public/assets/img/**/*'],
+    dist: './public/dist/img'
   }
-};
+}
+
+gulp.task('watch', (callback) => {
+  gulp.watch(paths.stylus.src, ['stylus'])
+  gulp.watch(paths.images.src, ['image'])
+})
+
+gulp.task('image', function () {
+  gulp.src(paths.images.src)
+    .pipe(image())
+    .pipe(gulp.dest(paths.images.dist));
+});
 
 gulp.task('stylus', () => {
   gulp.src(paths.stylus.src)
     .pipe(stylus({
       compress: true
     }))
-    .pipe(gulp.dest(paths.stylus.dist));
-});
+    .pipe(gulp.dest(paths.stylus.dist))
+})
 
-gulp.task('webpack:build', shell.task([
-  'npm run webpack:build'
-]));
-
-gulp.task('webpack:watch', shell.task([
-  'npm run webpack:watch'
-]));
-
-gulp.task('build', ['stylus']);
+gulp.task('dev', ['stylus', 'image', 'watch'])
